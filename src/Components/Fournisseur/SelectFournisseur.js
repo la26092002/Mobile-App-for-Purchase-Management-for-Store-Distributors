2e6d57300eed424995bc2b1bb0af50791cbbb949
la26092002 <larbibenyakhou@gmail.com>
2024-06-17 02:19:43 +0100

    l

---
+++ src/Components/Fournisseur/SelectFournisseur.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function SelectFournisseur() {
+  return (
+    <View>
+      <Text>SelectFournisseur</Text>
+    </View>
+  )
+}
--- src/Components/Vente/CreerVente.js
+++ src/Components/Vente/CreerVente.js
@@ -1,78 +1,79 @@
 import { View, Text, StyleSheet, ScrollView } from "react-native";
 import React, { useEffect, useState } from "react";
-import { Avatar, Button, Card, DataTable, TextInput } from "react-native-paper";
+import {
+  ActivityIndicator,
+  MD2Colors,
+  Avatar,
+  Button,
+  Card,
+  DataTable,
+  TextInput,
+} from "react-native-paper";
+
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 
-import { Dropdown } from "react-native-element-dropdown";
-import { MaterialIcons } from "@expo/vector-icons";
-import * as SQLite from "expo-sqlite";
-
 import { useTheme } from "react-native-paper";
 import { openDatabase } from "../../SqlLite";
 import AddVenteGrid from "./AddVenteGrid";
 import VenteSelect from "./VenteSelect";
+import SelectFournisseur from "../Fournisseur/SelectFournisseur";
 
 const LeftContent = (props) => (
   <Avatar.Icon {...props} icon="archive-arrow-down-outline" />
 );
 
-const data = [
-  { label: "Item 1", value: "1" },
-  { label: "Item 2", value: "2" },
-  { label: "Item 3", value: "3" },
-  { label: "Item 4", value: "4" },
-  { label: "Item 5", value: "5" },
-  { label: "Item 6", value: "6" },
-  { label: "Item 7", value: "7" },
-  { label: "Item 8", value: "8" },
-];
-
-export default function CreerVente({ setPart }) {
-  const [name, setName] = React.useState("");
-  const [price, setPrice] = React.useState("");
-
-  const [category, setCategorie] = useState(null);
-
-  const renderItem = (item) => {
-    return (
-      <View style={styles.item}>
-        <Text style={styles.textItem}>{item.label}</Text>
-        {item.value === category && (
-          <MaterialIcons name="category" size={20} color="black" />
-        )}
-      </View>
-    );
-  };
-
+export default function CreerVente() {
   let theme = useTheme();
-
-  useEffect(() => {
-    console.log(theme.colors.primary);
-  }, []);
-
-
-
+  const [status, setStatus] = useState(0);
+  let Suivant = async () => {
+    setStatus(1);
+    setTimeout(function() {
+      console.log("Waited 1 second");
+      setStatus(2);
+  }, 2000);
+  };
   return (
     <>
       <ScrollView style={styles.scrollView}>
-        <Card style={styles.Card}>
-          <Card.Title title="Ajouter une Vente" left={LeftContent} />
-          <AddVenteGrid />
-          <VenteSelect />
-          <Card.Actions>
-            <Button onPress={() => alert(name)}>Ajouter</Button>
-          </Card.Actions>
-        </Card>
-        
+        {status === 0 && (
+          <Card style={styles.Card}>
+            <Card.Title title="Ajouter une Vente" left={LeftContent} />
+            <AddVenteGrid />
+            <VenteSelect />
+            <Card.Actions>
+              <Button onPress={Suivant}>Suivant</Button>
+            </Card.Actions>
+          </Card>
+        )}
+        {status === 1 && (
+          <View style={styles.activityIndicatorContainer}>
+            <ActivityIndicator
+              animating={true}
+              size={"large"}
+              color={theme.colors.primary}
+            />
+          </View>
+        )}
+        {status === 2 && (
+          <>
+          <SelectFournisseur />
+          </>
+        )}
       </ScrollView>
     </>
   );
 }
 
 const styles = StyleSheet.create({
+  activityIndicatorContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    height: hp(80), // Adjust this value if needed
+  },
   scrollView: {
     height: hp(80),
   },
